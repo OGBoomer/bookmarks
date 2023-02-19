ec040db179fd937a169809c10959d88cce7a2ab8
mike on Desktop <mikemabe@att.net>
2023-02-19 00:26:53 -0600

    password change code

--- account/urls.py
+++ account/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     # new with login/logout
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.xhtml'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('password-change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('password-change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
     path('', views.dashboard, name='dashboard'),
+
 ]
